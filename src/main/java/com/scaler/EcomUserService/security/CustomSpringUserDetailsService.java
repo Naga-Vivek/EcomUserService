ccdc2a1b05c88a434f2f3f62af48c3d0856b8187
Naga-Vivek <nagavivek0798@gmail.com>
2024-03-24 13:01:30 +0530

    OAuth Implementation started

---
+++ src/main/java/com/scaler/EcomUserService/security/CustomSpringUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.scaler.EcomUserService.security;
+
+import com.scaler.EcomUserService.model.User;
+import com.scaler.EcomUserService.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomSpringUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public CustomSpringUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByEmail(username);
+        if(userOptional.isEmpty()){
+            throw new UsernameNotFoundException("User with given user name is not found");
+        }
+        User user = userOptional.get();
+        return new CustomSpringUserDetails(user);
+    }
+}
